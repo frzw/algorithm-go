82e1bb04664a8fbf274bc5c5d375d267506336af
lizhi <lizhi@qq.com>
2021-02-28 14:36:31 +0800

    ShellSort Done

---
+++ base/ShellSort.go
@@ -0,0 +1,49 @@
+package base
+
+/* 希尔排序 */
+
+// Asc：升序
+func ShellSortAsc(arr []int) []int {
+	length := len(arr)
+	gap := 1
+	for gap < length/3 {
+		gap = gap*3 + 1
+	}
+	for gap > 0 {
+		for i := gap; i < length; i++ {
+			temp := arr[i]
+			j := i - gap
+			for j >= 0 && arr[j] > temp {
+				arr[j+gap] = arr[j]
+				j -= gap
+			}
+			arr[j+gap] = temp
+		}
+		gap = gap / 3
+	}
+
+	return arr
+}
+
+// Desc：降序
+func ShellSortDesc(arr []int) []int {
+	length := len(arr)
+	gap := 1
+	for gap < length/3 {
+		gap = gap*3 + 1
+	}
+
+	for gap > 0 {
+		for i := gap; i < length; i++ {
+			temp := arr[i]
+			j := i - gap
+			for j >= 0 && arr[j] < temp {
+				arr[j+gap] = arr[j]
+				j -= gap
+			}
+			arr[j+gap] = temp
+		}
+		gap = gap / 3
+	}
+	return arr
+}
